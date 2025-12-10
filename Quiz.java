4f19bc0e87dd27499c2cbb337963269e7298a283
jessica she <157832275+jessicasshe@users.noreply.github.com>
2025-12-09 22:32:33 -0500

    Update Quiz.java
    - Fixed logic issues and structure

--- Quiz.java
+++ Quiz.java
@@ -1,177 +1,122 @@
-import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Quiz {
     
-    private List<Question> questions;
+    private ArrayList<Question> questions;
     //private int score;
     private Scanner scanner;
     private MainCharacter player;
-    //private int totalQuizAttempts;
-    //private final int maxQuizAttempts = 3;
-    //new attributes
-    private int userHP;
-    private int enemyHP;
-    private int userMaxHP;
-    private int enemyMaxHP;
-    private int userHitPerWrong;
-    private int enemyHitPerCorrect;
+    private Enemy enemy;
+    private final int maxQuizAttempts = 3; 
+    //private boolean failedAQuestion = false;
     
-    
-    public Quiz(Scanner theScanner, MainCharacter playerCharacter){
+    public Quiz(Scanner theScanner, MainCharacter playerCharacter, Enemy enemy){
         this.questions = new ArrayList<>();
         //this.score = 0;
         this.scanner = theScanner;
         this.player = playerCharacter;
+        this.enemy = enemy;
         //this.totalQuizAttempts = maxQuizAttempts;
     }
     
-    public void addQuestions(Question question){
+    public ArrayList<Question> addQuestion(Question question){
         questions.add(question);
+        return questions;
     }
-    //this is to help set up combat
-    public void setUpCombat(int userMax, int enemyMax, int damageToUser, int damageToEnemy){
-        this.userMaxHP = userMax;
-        this.enemyMaxHP = enemyMax;
-        this.userHitPerCorrect = damageToUser;
-        this.enemyHitPerCorrect = damageToEnemy;
-        this.userHP = userMax;
-        this.enemyHP = enemyMax;
     
-    public int startQuiz(){
-        System.out.println("Answer the questions to defeat the robots!!!");
-        
-        int quizScore = 0;
-        boolean allQuestionsCorrect = true;
-        
-        //if (totalQuizAttempts <= 0){
-            //System.out.println("No more quiz chances");
-            //return 0;
-        //}
-        
-        for (int i = 0; i < questions.size(); i++){
-            Question currentQuestion = questions.get(i);
-            boolean questionCorrectInCurrentAttempt = false;
-            
-            int initialAttempts = currentQuestion.getAttemptsRemaining();
-            
-            //System.out.println("\n Question "+(i+1)+" (Attempts left: "currentQuestion.getAttemptsRemaining()+"): ");
-            //currentQuestion.displayQuestion();
-            
-            if(currentQuestion.getAttemptsRemaining() <= 0){
-                System.out.println("Question "+(i+1)+" (Attempts left: none");
-                allQuestionsCorrect = false;
-                continue;
-            }
-            
-            while (currentQuestion.getAttemptsRemaining() > 0 && !questionCorrectInCurrentAttempt){
-                System.out.println("Question "+(i+1)+" (Attempts left: "+currentQuestion.getAttemptsRemaining()+"): ");
-                currentQuestion.displayQuestion();
-            
-            
-                int userAnswer = -1;
-                while (true){
-                    System.out.print("Enter your answer (1-"+currentQuestion.getOptions().size()+"): ");
-                    if (scanner.hasNextInt()){
-                        userAnswer = scanner.nextInt();
-                        if (userAnswer >= 1 && userAnswer <= currentQuestion.getOptions().size()){
-                            break;
-                        } else {
-                            System.out.println("Invalid.");
-                        }
-                    } else {
-                        System.out.println("Invalid.");
-                        scanner.next();
-                    }
-                }
-            
-                if (currentQuestion.isCorrect(userAnswer - 1)){
-                    System.out.println("correct!");
-                    enemyHP -= enemyHitPerCorrect;
-                    System.out.println("The AI took "+enemyHitPerCorrect+" damage! HP now: "+enemyHP);
-                    if (enemyHP <= 0){
-                        System.out.println("enemy defeated!");
-                        return 1;
-                    }
-                    if (currentQuestion.getAttemptsRemaining() == initialAttempts){
-                        quizScore++;
-                    }
-                    questionCorrectInCurrentAttempt = true;
-                } else {
-                    userHP -= userHitPerWrong;
-                    System.out.println("Incorrect. You took "+userHitPerWrong+" damage! HP now: " +userHP);
-                    if (userHP <= 0){
-                        System.out.println("You died. GAME OVER.");
-                        return 0;
-                    }
-                    currentQuestion.decrementAttempts();
-                    System.out.println("You have "+ currentQuestion.getAttemptsRemaining()+" attempts left");
-                    if (currentQuestion.getAttemptsRemaining() == 0){
-                        System.out.println("You've run out of attempts. GAME OVER.");
-                        allQuestionsCorrect = false;
-                    }
-                }
-                player.takeDamage(userDamagePerQuestion);
-                enemy.takeDamage(enemyDamagePerQuestion);
-
-                System.out.println("---STATUS---"); //player can clearly see current health and enemy health
-                System.out.println("Your HP: "player.getHealth()+"/"+player.getMaxHealth());
-                System.out.println("Enemy HP: "enemy.getHealth()+"/"enemy.getMaxHealth());
-                
-            }
-            if (!questionCorrectInCurrentAttempt && currentQuestion.getAttemptsRemaining() > 0){
-                allQuestionsCorrect = false;
-            }
-            
+    public ArrayList<Question> reset_all_question_attempts()
+    {
+        for(int i = 0; i < questions.size(); i++)
+        {
+            questions.get(i).resetAttempts();
         }
-    
-    //totalQuizAttempts--;
-    
-    System.out.println("Finished!");
-    System.out.println("Your score for this round is: "+quizScore+" out of "+questions.size());
-    
-    if (quizScore == questions.size() && allQuestionsCorrect){
-        return 1;
-    } else {
-        System.out.println("Your skills were lacking and you got questions wrong!");
-        System.out.println("You take 20 damage for getting the question wrong. Game over for this location");
-        return 0;
-    } // mc health managed by gamemanager - just for show here
-}
-    
-public static Quiz AbandonedOfficeQuiz(Scanner scanner, MainCharacter player){
-        Quiz quiz = new Quiz(scanner, player);
-        
-        List<String> options1 = List.of("int(num)","Double y = double(x)","int x = 4; double y = x;");
-        quiz.addQuestions(new Question("What is an example of implicit casting?", options1, 2));
-                
-        List<String> options2 = List.of("Inheritance","General Association","Composition");
-        quiz.addQuestions(new Question("Dog IS A Animal shows what type of relationship?", options2, 0));
-        
-        return quiz;
-    } 
-public static Quiz RobotFactoryQuiz(Scanner scanner, MainCharacter player){
-        Quiz quiz = new Quiz(scanner, player);
-        
-        List<String> options3 = List.of("Public void main Person{","public class Person{","Person{");
-        quiz.addQuestions(new Question("What is the correct way to code an object class?", options3, 1));
-                
-        List<String> options4 = List.of("for","while","do");
-        quiz.addQuestions(new Question("____(int i = 0;, i<p, i++){ --> What type of loop is this??", options4, 0));
-        
-        return quiz;
+        return questions;
     }
-public static Quiz GovHeadquartersQuiz(Scanner scanner, MainCharacter player){
-        Quiz quiz = new Quiz(scanner, player);
+    
+    public int startQuiz(int attempts){
         
-        List<String> options5 = List.of("O(n log n)"," O(n)"," O(n^2)");
-        quiz.addQuestions(new Question("What is the time complexity of a Merge Sort?", options5, 0));
-                
-        List<String> options6 = List.of("When you are trying to modify a value and already have a reference to the item","When you don't have a reference to the item and are trying to modify a value","When you are trying to search for a value");
-        quiz.addQuestions(new Question("When should a LinkList be used over an ArrayList?", options6, 0));
-        
-        return quiz;
-    }
 
+        // gamemanager controls quiz attempts, quiz controls question attempts
+
+        int quizScore = 0; 
+
+        for (int i = 0; i < questions.size(); i++){
+            Question currentQuestion = questions.get(i); 
+            
+            boolean question_correct = false;
+            int initialAttempts = currentQuestion.getAttemptsRemaining(); // default 3
+            
+        /*if(currentQuestion.getAttemptsRemaining() = 0){
+            System.out.println("Question "+(i+1)+" (Attempts left: none");
+            allQuestionsCorrect = false;
+            break;
+        }
+        */
+        
+            while (!question_correct){ 
+            
+            // check first if user has attempts remaining 
+            
+                if(currentQuestion.getAttemptsRemaining() == 0)
+                {
+                    return 0; // failed battle 
+                }
+            
+            else
+            {
+                System.out.println("Question "+(i+1)+" (Attempts left: "+currentQuestion.getAttemptsRemaining()+") ");
+
+                System.out.println(currentQuestion.getQuestionText()); //option
+                
+                ArrayList<Integer> option_numbers = new ArrayList<>();
+                for (int j = 0; j < currentQuestion.getOptions().size(); j++){
+                        System.out.println((j + 1)+". " + currentQuestion.getOptions().get(j));
+                        option_numbers.add(j+1);
+                    }
+                    
+                // validate user option
+                int userAnswer = GameManager.validateIntInput("Enter your answer (1-"+currentQuestion.getOptions().size()+"):", option_numbers);
+
+                
+                if (currentQuestion.isCorrect(userAnswer - 1)){
+                    System.out.println("Answer correct!");
+                    
+                    if (currentQuestion.getAttemptsRemaining() == initialAttempts && attempts == 0){ 
+                        quizScore++;
+                        player.updateScore(1); // only increase score if u didnt try again
+                    }
+                    
+                    if(enemy != null){
+                        player.attack(enemy.damage_taken_per_ans(), enemy);
+                    }
+                
+               /* if(!player.getCurrentLocation().equals("Government Headquarters"))
+                {
+                    player.attack(enemy.getStartingHealth()/2, enemy);
+                }
+                */
+
+                } 
+                else {
+                    currentQuestion.decrementAttempts();
+                    System.out.println("Incorrect. You have "+ currentQuestion.getAttemptsRemaining()+ " attempts remaining.");
+                    if(enemy != null)
+                    {enemy.attack(player.damage_taken_per_ans(), player);
+                    }
+                }
+                
+                if(enemy!=null)
+                {
+                    System.out.println("Enemy HP: "+enemy.getHealth()+"/"+enemy.getStartingHealth());
+                }
+                System.out.println("Your HP: "+player.getHealth() + "/"+player.getStartingHealth());
+                question_correct = true;
+
+            }
+        }
+    }
+    // reached end of quiz
+    return quizScore;
+}
 }
